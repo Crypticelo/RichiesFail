b656bd25ea2a4e16dff5b2220053866e087954bb
Crypticelo <jusanes04@gmail.com>
2024-04-11 14:21:45 +0200

    Added Pages

--- pages/index.vue
+++ pages/index.vue
@@ -9,43 +9,15 @@
       class="rounded-borders"
     >
       <q-carousel-slide name="style" class="column no-wrap flex-center">
-        <q-icon name="style" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
+        <img src="/image/first.png" alt="Style" />
       </q-carousel-slide>
       <q-carousel-slide name="tv" class="column no-wrap flex-center">
-        <q-icon name="live_tv" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
+        <img src="/image/second.png" alt="TV" />
       </q-carousel-slide>
       <q-carousel-slide name="layers" class="column no-wrap flex-center">
-        <q-icon name="layers" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="map" class="column no-wrap flex-center">
-        <q-icon name="terrain" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
+        <img src="/image/third.png" alt="Layers" />
       </q-carousel-slide>
     </q-carousel>
-
-    <div class="row justify-center">
-      <q-btn-toggle
-        glossy
-        v-model="slide"
-        :options="[
-          { label: 1, value: 'style' },
-          { label: 2, value: 'tv' },
-          { label: 3, value: 'layers' },
-          { label: 4, value: 'map' },
-        ]"
-      />
-    </div>
   </div>
 </template>
 
@@ -59,7 +31,7 @@ export default {
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo provident incidunt ducimus iusto perferendis porro earum. Totam, numquam?"
     );
 
-    const slides = ref(["style", "tv", "layers", "map"]);
+    const slides = ref(["style", "tv", "layers"]);
     let intervalId: any;
 
     const advanceSlide = () => {
