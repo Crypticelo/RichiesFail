375c84bb9310f14577b78e828fa24b761a1d251c
don.cryptus <don.cryptus@gmail.com>
2024-04-11 14:06:32 +0200

    x

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,85 @@
 <template>
-  <div>
-    <h1 class="text-3xl">Test Page</h1>
+  <div class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      animated
+      control-color="primary"
+      class="rounded-borders"
+    >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-icon name="style" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-icon name="terrain" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+
+    <div class="row justify-center">
+      <q-btn-toggle
+        glossy
+        v-model="slide"
+        :options="[
+          { label: 1, value: 'style' },
+          { label: 2, value: 'tv' },
+          { label: 3, value: 'layers' },
+          { label: 4, value: 'map' },
+        ]"
+      />
+    </div>
   </div>
 </template>
-0
+
+<script lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+
+export default {
+  setup() {
+    const slide = ref("style");
+    const lorem = ref(
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo provident incidunt ducimus iusto perferendis porro earum. Totam, numquam?"
+    );
+
+    const slides = ref(["style", "tv", "layers", "map"]);
+    let intervalId: any;
+
+    const advanceSlide = () => {
+      const currentIndex = slides.value.indexOf(slide.value);
+      const nextIndex = (currentIndex + 1) % slides.value.length;
+      slide.value = slides.value[nextIndex];
+    };
+
+    onMounted(() => {
+      intervalId = setInterval(advanceSlide, 3000); // changes slide every 3000 milliseconds (3 seconds)
+    });
+
+    onUnmounted(() => {
+      clearInterval(intervalId);
+    });
+
+    return {
+      slide,
+      lorem,
+    };
+  },
+};
+</script>
